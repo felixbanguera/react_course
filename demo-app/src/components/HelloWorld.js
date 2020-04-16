190b59be54347d096ac69a8769d2ccdd4c98a920
Felix Banguera <fbanguera@weedmaps.com>
2020-04-16 11:12:57 -0500

    Udemy Course [https://www.udemy.com/course/getting-started-with-react] Section 1: Getting Started
    NOTES: I commented out all defaults needed so I could follow up the course.

---
+++ demo-app/src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// The following is code from the course
+export const HelloWorld = () => { 
+  // return React.createElement('h1', null, 'Hello to Covid World!');
+  return  <>
+    <h1>Hello to Coronavirus World!</h1>
+    <span>Test span</span>
+  </>;
+}
+  
